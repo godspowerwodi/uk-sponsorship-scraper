bef1832c48220fa05df7091b0bb14920915e67cc
Godspower Wodi <gp.woody58@gmail.com>
2026-09-19 12:43:45 +0100

    fix: update tinytoolz URL to use onrender.com domain

--- sponsorship-scout/src/sponsorship_scout/ui/public_app.py
+++ sponsorship-scout/src/sponsorship_scout/ui/public_app.py
@@ -274,7 +274,7 @@ if scan_button:
                         if score < 65:
                             import urllib.parse
                             encoded_url = urllib.parse.quote_plus(job.get('url') or '')
-                            job['Boost ATS Score'] = f"https://tinytoolzhub.org/ats-matcher?utm_source=sponsorship_scout_table&cv_id={cv_uuid}&job_url={encoded_url}"
+                            job['Boost ATS Score'] = f"https://tinytoolz-hub.onrender.com/ats-matcher?utm_source=sponsorship_scout_table&cv_id={cv_uuid}&job_url={encoded_url}"
                         else:
                             job['Boost ATS Score'] = None
                         
